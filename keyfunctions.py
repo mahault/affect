a31ebb42da0c573a615bba194020e0ed45334726
mahault <42900146+mahault@users.noreply.github.com>
2024-04-09 10:50:00 -0400

    creating the graoh

--- keyfunctions.py
+++ keyfunctions.py
@@ -44,3 +44,29 @@ def generate_connected_clusters( # 'cluster_size' refers to how many rooms you w
             for i, loc in enumerate(graph.nodes)
         ]
     } # returns a dictionary of locations we have (location 0, location 1, etc) as rooms and hallway indices (hallway 0, room 1, room 2, etc)
+
+# CREATING THE GRAPH/ENVIRONMENT FOR THE WALLETFINDING-ONLY AND EMOTIONAL-WALLETFINDING TASK - ADAPTED OZAN FUNCTION
+def house(cluster_size = 5, connections = 4) -> typing.Tuple[networkx.Graph, dict]:
+    edges = []
+    connecting_node = 0
+    hallway_names = ["hall", "kitchen", "bedroom", "bathroom", "gameroom", "dining room", "library"]  # add more names if needed
+    hallway_counter = 0 
+    while connecting_node < connections * cluster_size:
+        edges += [
+            (connecting_node, a)
+            for a in range(
+                connecting_node + 1, connecting_node + cluster_size + 1
+            )
+        ]
+        connecting_node = len(edges)
+    graph = networkx.Graph()
+    graph.add_edges_from(edges)
+    locations = []
+    for i, loc in enumerate(graph.nodes):
+        if len(list(graph.neighbors(loc))) > 1:
+            location = f"{hallway_names[hallway_counter]} ({i})"
+            hallway_counter += 1
+        else: 
+            location = f"drawer {i}"
+        locations.append(location)
+    return graph, {"locations": locations} 
